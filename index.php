09699c96537b86718e1f5200457ab5c0fa5832b6
Carlos <eicarlos@outlook.com.br>
2019-09-14 18:30:50 -0300

    adicionando Título, tabela e cabeçalho do calendário.

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Calendário</title>
 </head>
 <body>
-    
+    <h1><?php echo "Calendário"; ?></h1>
+    <table border="1">
+        <tr>
+            <th>Dom</th>
+            <th>Seg</th>
+            <th>Ter</th>
+            <th>Qua</th>
+            <th>Qui</th>
+            <th>Sex</th>
+            <th>Sáb</th>
+        </tr>
+    </table>
 </body>
 </html>
