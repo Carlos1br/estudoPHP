4b9fc19cff9370498ecdd90fa81e260f95fe10e1
Carlos <eicarlos@outlook.com.br>
2019-09-18 10:28:16 -0300

    Criando pagima tarefas e estrutur inicial

---
+++ tarefas/tarefas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gerenciador de tarefas</title>
+</head>
+<body>
+    <h1>Gerenciador de tarefas</h1>
+    <form action="POST">
+        <fieldset>
+            <legend>Nova tarefa</legend>
+            <label for="nome">
+            tarefa:
+            <input type="text" name="nome" id="nome" placeholder="João Exemplo Da Silva">
+            </label>
+            <input type="submit" value="Cadastrar">
+        </fieldset>
+    </form>
+</body>
+</html>
